Fix wrong dimension input

dagan_trainer.py
@@ -241,7 +241,9 @@ class DaganTrainer:
         val_img = self.sample_val_images(val_dataloader)
         self.g.eval()
         with torch.no_grad():
-            generated_val_img = self.sample_generator(self.display_transform(val_img['original'])).cpu()
+            generated_val_img = self.sample_generator(
+                self.display_transform(val_img['original'][None, :])
+            ).cpu()
         self.g.train()
 
         self.visualizer.log_generations(