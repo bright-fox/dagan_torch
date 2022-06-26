c08bfb04127cea67efe58f5dc99d8a4c215738f8
Hung Vu <leertet96@gmail.com>
2022-06-27 01:43:44 +0200

    Reformat print line

--- dagan_trainer.py
+++ dagan_trainer.py
@@ -236,7 +236,7 @@ class DaganTrainer:
 
     def log_losses(self):
         if self.num_steps % 100 == 0:
-            print(f'\n[INFO] Iteration {self.num_steps}\n:')
+            print(f'[INFO] Iteration {self.num_steps}')
             self.visualizer.log_losses(self.losses)
 
 
