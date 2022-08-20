0199b695cbf718c029b68463a82c36c6182ac4a1
bright-fox <leertet96@gmail.com>
2022-08-20 20:58:38 +0200

    Refactor dataset.py

--- dataset.py
+++ dataset.py
@@ -4,25 +4,26 @@ import warnings
 
 
 class DaganDataset(Dataset):
-    """Face Landmarks dataset."""
+    """
+    Dataset that contains original images and its corresponding augmented image
+    """
 
-    def __init__(self, originals, augmentations, transform=None):
+    def __init__(self, originals, augmentations):
         assert len(originals) == len(augmentations)
         self.originals = originals
         self.augmentations = augmentations
-        self.transform = transform
 
     def __len__(self):
         return len(self.originals)
 
     def __getitem__(self, idx):
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", category=UserWarning)
-            return self.transform(self.originals[idx]), self.transform(
-                self.augmentations[idx]
-            )
+        """
+        Scales original and augmentation to [0, 1] and afterwards normalize it to [-1, 1] and returns
+        images as a tuple (original, augmentation)
+        """
+        # scale to [0, 1], then normalize to [-1, 1]
+        return ((self.originals[idx] / 255) - 0.5) / 0.5, ((self.augmentations[idx] / 255) - 0.5) / 0.5
 
-
-def create_dagan_dataloader(originals, augmentations, transform, batch_size):
-    train_dataset = DaganDataset(originals, augmentations, transform)
+def create_dagan_dataloader(originals, augmentations, batch_size):
+    train_dataset = DaganDataset(originals, augmentations)
     return DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
