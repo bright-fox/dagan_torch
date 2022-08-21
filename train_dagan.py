17dda024ae7b4aeb2be5f479f966203f3a7c4d2a
bright-fox <leertet96@gmail.com>
2022-08-21 12:47:36 +0200

    Refactor train_dagan

--- train_dagan.py
+++ train_dagan.py
@@ -1,3 +1,4 @@
+from turtle import pd
 from dagan_trainer import DaganTrainer
 from discriminator import Discriminator
 from generator import Generator
@@ -19,74 +20,48 @@ torch.backends.cudnn.benchmark = False
 
 # Load input args
 args = get_dagan_args()
-
-# init wandb
-visualizer = Visualizer(args)
-
-# get the data set paths
-dataset_path = args.dataset_path
-train_dataset_path = f'{dataset_path}/train.npz'
-test_dataset_path = f'{dataset_path}/test.npz'
-val_dataset_path = f'{dataset_path}/val.npz'
-
-# load the data
-train_data = np.load(train_dataset_path)
-val_data = np.load(val_dataset_path)
-
 final_generator_path = args.final_model_path
-save_checkpoint_path = args.save_checkpoint_path
-load_checkpoint_path = args.load_checkpoint_path
-in_channels = train_data['orig'].shape[-1]
-img_size = args.img_size or train_data['orig'].shape[2]
-batch_size = args.batch_size
-epochs = args.epochs
-dropout_rate = args.dropout_rate
-max_pixel_value = args.max_pixel_value
-should_display_generations = not args.suppress_generations
 
 # Input sanity checks
 final_generator_dir = os.path.dirname(final_generator_path) or os.getcwd()
 if not os.access(final_generator_dir, os.W_OK):
     raise ValueError(final_generator_path + " is not a valid filepath.")
 
-g = Generator(dim=img_size, channels=in_channels, dropout_rate=dropout_rate)
-d = Discriminator(dim=img_size, channels=in_channels * 2, dropout_rate=dropout_rate)
+# init wandb
+visualizer = Visualizer(args)
 
-mid_pixel_value = max_pixel_value / 2
-train_transform = transforms.Compose(
-    [
-        transforms.ToPILImage(),
-        transforms.Resize(img_size),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            (mid_pixel_value,) * in_channels, (mid_pixel_value,) * in_channels # mean, standard deviation
-        ),
-    ]
-)
+# load the data
+train_data = np.load(f'{args.dataset_path}/train.npz')
+val_data = np.load(f'{args.dataset_path}/val.npz')
+train_dataloader = create_dagan_dataloader(train_data['orig'], train_data['aug'], args.batch_size)
+val_dataloader = create_dagan_dataloader(val_data['orig'], val_data['aug'], args.batch_size)
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-train_dataloader = create_dagan_dataloader(train_data['orig'], train_data['aug'], train_transform, batch_size)
-val_dataloader = create_dagan_dataloader(val_data['orig'], val_data['aug'], train_transform, batch_size)
+# get img info
+in_channels = train_data['orig'].shape[1]
+img_size = args.img_size or train_data['orig'].shape[2]
 
+# init networks and corresponding optimizers
+g = Generator(dim=img_size, channels=in_channels, dropout_rate=args.dropout_rate)
+d = Discriminator(dim=img_size, channels=in_channels * 2, dropout_rate=args.dropout_rate)
 g_opt = optim.Adam(g.parameters(), lr=0.0001, betas=(0.0, 0.9))
 d_opt = optim.Adam(d.parameters(), lr=0.0001, betas=(0.0, 0.9))
 
-display_transform = train_transform
-
+# train the models
 trainer = DaganTrainer(
     generator=g,
     discriminator=d,
     gen_optimizer=g_opt,
     dis_optimizer=d_opt,
-    batch_size=batch_size,
+    batch_size=args.batch_size,
     visualizer=visualizer,
-    device=device,
+    device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
     critic_iterations=5,
-    # save_checkpoint_path=save_checkpoint_path,
-    # load_checkpoint_path=load_checkpoint_path,
-    display_transform=display_transform,
 )
-trainer.train(data_loader=train_dataloader, epochs=epochs, val_dataloader=val_dataloader)
+trainer.train(
+    data_loader=train_dataloader,
+    epochs=args.epochs,
+    val_dataloader=val_dataloader,
+)
 
 # Save final generator model
 torch.save(trainer.g, f'{final_generator_path}/{args.name}_model.pt')
