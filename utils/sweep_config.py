db60648fca3d855a8d1639947c4ae569709d9e48
bright-fox <leertet96@gmail.com>
2022-11-07 23:57:49 +0100

    Add sweep config

---
+++ utils/sweep_config.py
@@ -0,0 +1,17 @@
+batch_config = {
+    'method': 'grid',
+    'metric': {
+        'name': 'G',
+        'goal': 'minimize',
+    },
+    'parameters': {
+        'detach': {
+            'values': [
+                {}, # no detach
+                {'disc': 3}, # only disc
+                {'gen': 3, 'disc': 3, 'noise': 2}, # all detach
+            ],
+        },
+        'gp_weight': [0, 2]
+    }
+}
