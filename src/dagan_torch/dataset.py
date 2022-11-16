cf667b4651dd7b6240ef39ebe804e38a72012ef9
bright-fox <leertet96@gmail.com>
2022-11-16 21:06:40 +0100

    Add already normalized dataset

--- src/dagan_torch/dataset.py
+++ src/dagan_torch/dataset.py
@@ -21,6 +21,29 @@ class DaganDataset(Dataset):
         # scale to [0, 1], then normalize to [-1, 1]
         return ((self.originals[idx] / 255) - 0.5) / 0.5, ((self.augmentations[idx] / 255) - 0.5) / 0.5
 
+class DaganNormalizedDataset(Dataset):
+    """
+    Dataset that contains original images and its corresponding augmented image (already normalized to [-1,1])
+    """
+
+    def __init__(self, originals, augmentations):
+        assert len(originals) == len(augmentations)
+        self.originals = originals
+        self.augmentations = augmentations
+
+    def __len__(self):
+        return len(self.originals)
+
+    def __getitem__(self, idx):
+        """
+        """
+        # scale to [0, 1], then normalize to [-1, 1]
+        return self.originals[idx], self.augmentations[idx]
+
+def create_normalized_dl(originals, augmentations, batch_size):
+    train_data = DaganNormalizedDataset(originals, augmentations)
+    return DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=1)
+
 def create_dl(originals, augmentations, batch_size):
     train_dataset = DaganDataset(originals, augmentations)
     return DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
