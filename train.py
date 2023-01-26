0cd08f1f7cbcc75075796a8010946d2309c39be3
bright-fox <leertet96@gmail.com>
2023-01-26 18:03:51 +0100

    Refactor train script

--- train.py
+++ train.py
@@ -2,12 +2,13 @@ from dagan_trainer import DaganTrainer
 from dagan_torch.discriminator import Discriminator
 from dagan_torch.generator import Generator
 from dagan_torch.dataset import create_dl
-from utils.parser import get_dagan_args
+from utils.parser import get_dagan_args, prepare_args
 import torch
 import os
 import torch.optim as optim
 import numpy as np
 from utils.logger import Logger
+from utils.utils import create_output_dirs
 
 # To maintain reproducibility
 torch.manual_seed(0)
@@ -20,21 +21,18 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Load input args
 args = get_dagan_args()
-final_generator_path = args.final_model_path
+args = prepare_args(args)
 
-# Input sanity checks
-final_generator_dir = os.path.dirname(final_generator_path) or os.getcwd()
-if not os.access(final_generator_dir, os.W_OK):
-    raise ValueError(final_generator_path + " is not a valid filepath.")
+model_path, val_path = create_output_dirs(args.model_path, args.name, is_sweep=False)
 
 # init wandb
-visualizer = Logger(args)
+logger = Logger(args)
 
 # load the data
 train_data = np.load(f'{args.dataset_path}/train.npz')
 val_data = np.load(f'{args.dataset_path}/val.npz')
-train_dataloader = create_dl(train_data['orig'], train_data['aug'], args.batch_size)
-val_dataloader = create_dl(val_data['orig'], val_data['aug'], args.batch_size)
+train_dl = create_dl(train_data['orig'], train_data['aug'], args.batch_size)
+val_dl = create_dl(val_data['orig'], val_data['aug'], args.batch_size)
 
 # get img info
 in_channels = train_data['orig'].shape[1]
@@ -52,16 +50,20 @@ trainer = DaganTrainer(
     discriminator=d,
     gen_optimizer=g_opt,
     dis_optimizer=d_opt,
-    logger=visualizer,
+    logger=logger,
     device=device,
     critic_iterations=5,
 )
 trainer.train(
-    data_loader=train_dataloader,
+    data_loader=train_dl,
     epochs=args.epochs,
-    val_dataloader=val_dataloader,
+    val_dataloader=val_dl,
 )
 
 # Save final generator model
-torch.save(trainer.g, f'{final_generator_path}/{args.name}_model.pt')
-torch.save(trainer.g.state_dict(), f'{final_generator_path}/{args.name}_state_dict.pt')
+torch.save(trainer.g, os.path.join(model_path, 'model.pt'))
+torch.save(trainer.g.state_dict(), os.path.join(model_path, 'state_dict.pt'))
+
+# Save the discriminator model
+torch.save(trainer.d, os.path.join(model_path, 'disc.pt'))
+torch.save(trainer.d.state_dict(), os.path.join(model_path, 'disc_state_dict.pt'))
